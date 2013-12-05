c9b5d92a42e9b042ecfd9dcb4b5f2f8cdbde8405
Abigail B Klein <kleinab@mit.edu>
2013-12-04 23:58:45 -0500

    fixing the vertex and face calculations

--- data/script2.py
+++ data/script2.py
@@ -1,6 +1,6 @@
-i=0
-left=[]
-right=[]
+i=1
+left=[0]
+right=[0]
 f=open("Model1.attach", "r")
 for line in f:
   if '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 ' in line:
@@ -9,9 +9,9 @@ for line in f:
     right+=[i]
   i+=1
 
-j=0
-l=0
-r=0
+j=1
+l=1
+r=1
 f2=open("Model1.obj", "r")
 fa=open("vertices-all.txt", "w")
 fl=open("vertices-left.txt", "w")
@@ -20,17 +20,17 @@ for line2 in f2:
   if l < len(left) and left[l] == j:
     fa.write(line2)
     fl.write(line2)
-    fr.write("v 0 0 0\n")
+    fr.write("v 0 0 0\n")#0
     l+=1
   elif r < len(right) and right[r] == j:
     fa.write(line2)
     fr.write(line2)
-    fl.write("v 0 0 0\n")
+    fl.write("v 0 0 0\n")#0
     r+=1
   elif line2[0] == 'v':
-    fa.write("v 0 0 0\n")
-    fr.write("v 0 0 0\n")
-    fl.write("v 0 0 0\n")
+    fa.write("v 0 0 0\n")#0
+    fr.write("v 0 0 0\n")#0
+    fl.write("v 0 0 0\n")#0
   elif line2[0] == 'f':
     split = line2.split()
     if int(split[1]) in left and int(split[2]) in left and int(split[3]) in left:
